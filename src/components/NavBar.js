0eb4230c6eda35e1ff264eca33cd2f3c6f5b1f7a
tobuya <thomasobuya95@gmail.com>
2023-06-15 22:21:39 +0300

    Add the navigation component

---
+++ src/components/NavBar.js
@@ -0,0 +1,67 @@
+import { useState, useEffect } from 'react';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import logo from '../assets/logo.svg';
+import linkedin from '../assets/linkedin.svg';
+import github from '../assets/github.svg';
+import medium from '../assets/medium.svg';
+
+const NavBar = () => {
+  const [activeLink, setActiveLink] = useState('home');
+  const [scrolled, setScrolled] = useState(false);
+
+  useEffect(() => {
+    const onScroll = () => {
+      if (window.scrollY > 50) {
+        setScrolled(true);
+      } else {
+        setScrolled(false);
+      }
+    };
+    window.addEventListener('scroll', onScroll);
+    return () => window.removeEventListener('scroll', onScroll);
+  }, []);
+
+  const onUpdateActiveLink = (link) => {
+    setActiveLink(link);
+  };
+
+  return (
+    <Navbar expand="lg" className={scrolled ? 'scrolled' : ''}>
+      <Container>
+        <Navbar.Brand href="#home">
+          <img src={logo} alt="logo" />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav">
+          <span className="navbar-toggler-icon" />
+        </Navbar.Toggle>
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home" className={activeLink === 'home' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('home')}>
+              Home
+            </Nav.Link>
+            <Nav.Link href="#projects" className={activeLink === 'projects' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('projects')}>
+              Projects
+            </Nav.Link>
+            <Nav.Link href="#about" className={activeLink === 'about' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('about')}>
+              About
+            </Nav.Link>
+          </Nav>
+          <span className="navbar-text">
+            <div className="social-links">
+              <a href="https://www.linkedin.com/in/tobuya/"><img src={linkedin} alt="LinkedIn" /></a>
+              <a href="https://github.com/tobuya"><img src={github} alt="GitHub" /></a>
+              <a href="https://medium.com/@tobuya"><img src={medium} alt="Medium" /></a>
+            </div>
+            <button type="button" className="vvd" onClick={() => console.log('connect')}>
+              <span>Let&apos;s Connect</span>
+            </button>
+          </span>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default NavBar;
