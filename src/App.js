442f882b09ef73039d7d4f6e1b1c6895a750e9f7
tobuya <thomasobuya95@gmail.com>
2023-06-15 22:24:14 +0300

    Import and render the navbar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import './App.css';
+import NavBar from './components/NavBar';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <div className="App" />
+    <div className="App">
+      <NavBar />
+    </div>
   );
 }
 
