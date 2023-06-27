e6bcd9d1e146a3cc120a5bdc9093c78d7509b44c
tobuya <thomasobuya95@gmail.com>
2023-06-27 23:05:40 +0300

    Modify the Banner component information

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,78 +1,53 @@
-import { useState, useEffect } from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
-import { ArrowRightCircle } from 'react-bootstrap-icons';
 import header from '../assets/header-img.svg';
+import linkedin from '../assets/linkedin.svg';
+import github from '../assets/github.svg';
+import medium from '../assets/medium.svg';
+import wave from '../assets/wave.png';
 
-const Banner = () => {
-  const [loopNum, setLoopNum] = useState(0);
-  const [isDeleting, setIsDeleting] = useState(false);
-  const toRotate = ['Software Developer', 'Data Analyst'];
-  const [text, setText] = useState('');
-  const [delta, setDelta] = useState(300 - Math.random() * 100);
-  const period = 2000;
-
-  const tick = () => {
-    const i = loopNum % toRotate.length;
-    const fullText = toRotate[i];
-    const updatedText = isDeleting
-      ? fullText.substring(0, text.length - 1) : fullText.substring(0, text.length + 1);
-
-    setText(updatedText);
-
-    if (isDeleting) {
-      setDelta((prevDelta) => prevDelta / 2);
-    }
-
-    if (!isDeleting && updatedText === fullText) {
-      setIsDeleting(true);
-      setDelta(period);
-    } else if (isDeleting && updatedText === '') {
-      setIsDeleting(false);
-      setLoopNum(loopNum + 1);
-      setDelta(500);
-    }
-  };
-
-  useEffect(() => {
-    const ticker = setInterval(() => {
-      tick();
-    }, delta);
-
-    return () => {
-      clearInterval(ticker);
-    };
-  }, [delta, text]);
-
-  return (
-    <section className="banner" id="home">
-      <Container>
-        <Row className="align-items-center">
-          <Col xs={12} md={6} xl={7}>
-            <div className="banner-content">
-              <h2 className="tagline">Hi I&apos;m Thomas Obuya</h2>
-              <h1>
-                <span className="wrap">{text}</span>
-              </h1>
-              <p>
-                I can help you build a product , feature or website.
-                Look through some of my work and experience!
-                If you like what you see and have a project you need coded,
-                don’t hesitate to contact me.
-              </p>
-              <button type="button" onClick={() => console.log('connect')}>
-                Let&apos;s connect
-                {' '}
-                <ArrowRightCircle size={25} />
-              </button>
+const Banner = () => (
+  <section className="banner" id="home">
+    <Container>
+      <Row className="align-items-center">
+        <Col xs={12} md={6} xl={7}>
+          <div className="banner-content">
+            <h1>
+              Hi there
+              {' '}
+              <img src={wave} alt="Wave" />
+              {' '}
+              <br />
+              I&apos;m Thomas Obuya
+            </h1>
+            <h2>Software Developer</h2>
+            <p>
+              Welcome to my portfolio!
+              As a highly skilled and adaptable junior software developer,
+              I specialize in Full-stack Web Development and Data Analysis.
+              I possess a strong passion for creating impactful web applications
+              and uncovering valuable insights from data.
+              Let&apos;s collaborate and bring your project to life!
+              Explore my work and discover how I can contribute to your success.
+            </p>
+            <div className="social-links social-icon">
+              <a href="https://www.linkedin.com/in/tobuya/" target="_blank" rel="noopener noreferrer">
+                <img src={linkedin} alt="LinkedIn" />
+              </a>
+              <a href="https://github.com/tobuya" target="_blank" rel="noopener noreferrer">
+                <img src={github} alt="GitHub" />
+              </a>
+              <a href="https://medium.com/@tobuya" target="_blank" rel="noopener noreferrer">
+                <img src={medium} alt="Medium" />
+              </a>
             </div>
-          </Col>
-          <Col xs={12} md={6} xl={5}>
-            <img src={header} alt="Header" />
-          </Col>
-        </Row>
-      </Container>
-    </section>
-  );
-};
+          </div>
+        </Col>
+        <Col xs={8} md={6} xl={5}>
+          <img src={header} alt="Header" />
+        </Col>
+      </Row>
+    </Container>
+  </section>
+);
 
 export default Banner;
