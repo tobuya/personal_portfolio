8dc0adbeab619a980acd8073f518e3384af5ebbf
tobuya <thomasobuya95@gmail.com>
2023-06-29 21:39:26 +0300

    Import components

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import './App.css';
 import NavBar from './components/NavBar';
 import Banner from './components/Banner';
 import Skills from './components/Skills';
+import Projects from './components/Projects';
+import About from './components/About';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
@@ -10,6 +14,10 @@ function App() {
       <NavBar />
       <Banner />
       <Skills />
+      <Projects />
+      <About />
+      <Contact />
+      <Footer />
     </div>
   );
 }
