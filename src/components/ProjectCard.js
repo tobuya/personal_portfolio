6b53c332be88fac8e8d0b27acde07d1975fc98ff
tobuya <thomasobuya95@gmail.com>
2023-07-01 23:41:25 +0300

    Refactor the code to use the ProjectDetails component

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,39 +1,39 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Col, Button } from 'react-bootstrap';
-import { v4 as uuidv4 } from 'uuid';
 
-const ProjectCard = ({
-  projectName, description, featuredImage, technologies, live, source,
-}) => (
-  <Col sm={6} md={4}>
-    <div className="proj-imgbx" style={{ backgroundImage: `url(${featuredImage})` }}>
-      <h3 className="proj-title">{projectName}</h3>
-      <p>{description}</p>
-      <ul className="proj-technologies">
-        {technologies.map((technology) => (
-          <li key={uuidv4()}>{technology}</li>
-        ))}
-      </ul>
-      <div className="btn-live-source-container">
-        <Button className="btn-live" href={live} target="_blank" rel="noopener noreferrer">
-          Live
-        </Button>
-        <Button className="btn-source" href={source} target="_blank" rel="noopener noreferrer">
-          Source
-        </Button>
+const ProjectCard = ({ project, openDetail }) => {
+  const {
+    projectName, introduction, featuredImage,
+  } = project;
+
+  const handleOpenDetail = () => {
+    openDetail(project);
+  };
+
+  return (
+    <main>
+      <div className="project-card">
+        <h2 className="proj-title">{projectName}</h2>
+        <img className="featured-image" src={featuredImage} alt={projectName} />
+        <p>{introduction}</p>
+        <button type="button" className="close-button" onClick={handleOpenDetail}>
+          Read more ...
+        </button>
       </div>
-    </div>
-  </Col>
-);
+    </main>
+  );
+};
 
 ProjectCard.propTypes = {
-  projectName: PropTypes.string.isRequired,
-  description: PropTypes.string.isRequired,
-  featuredImage: PropTypes.string.isRequired,
-  technologies: PropTypes.arrayOf(PropTypes.string).isRequired,
-  live: PropTypes.string.isRequired,
-  source: PropTypes.string.isRequired,
+  project: PropTypes.shape({
+    projectName: PropTypes.string.isRequired,
+    introduction: PropTypes.string.isRequired,
+    featuredImage: PropTypes.string.isRequired,
+    technologies: PropTypes.arrayOf(PropTypes.string).isRequired,
+    live: PropTypes.string.isRequired,
+    source: PropTypes.string.isRequired,
+  }).isRequired,
+  openDetail: PropTypes.func.isRequired,
 };
 
 export default ProjectCard;
